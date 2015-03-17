120b810809d1eb3c41f2560dddcd39eb9487cce9
Jimmy Gong <gong.jim@gmail.com>
2015-03-17 13:39:29 -0700

    Adds default props to prevent map from potentially being invoked on an undefined array

--- src/js/User.js
+++ src/js/User.js
@@ -27,6 +27,9 @@ var AddFriend = React.createClass({
       newFriend: ''
     };
   },
+  propTypes: {
+    addNew: React.PropTypes.func.isRequired
+  },
   updateNewFriend: function(e){
     this.setState({
       newFriend: e.target.value
@@ -50,6 +53,11 @@ var AddFriend = React.createClass({
 
 //TODO: Add remove friend capability next to each name
 var ShowList = React.createClass({
+  getDefaultProps: function(){
+    return {
+      names: []
+    };
+  },
   render: function(){
     var listItems = this.props.names.map(function(item){
       return <li>{item}</li>;
