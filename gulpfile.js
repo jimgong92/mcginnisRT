105d8b64dc8d054591c5a8d42c16d101ee30932d
Jimmy Gong <gong.jim@gmail.com>
2015-03-19 18:32:45 -0700

    Refactors gulp dev tasks to use browserify/watchify/reactify instead of various gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,63 @@
 var gulp = require('gulp');
 
+/**
+ * Utilities
+ */
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
 var plugins = require('gulp-load-plugins')();
 var path = {
   HTML: 'src/index.html',
-  ALL: ['src/js/*.js', 'src/js/**/*.js', 'src/index.html'],
-  JS: ['src/js/*.js', 'src/js/**/*.js'],
+  OUT: 'build.js',  
   MINIFIED_OUT: 'build.min.js',
+  DEST: 'dist',
   DEST_SRC: 'dist/src',
   DEST_BUILD: 'dist/build',
-  DEST: 'dist'
-}
+  ENTRY_POINT: './src/js/App.js'
+};
 
 /**
  * Development tasks
  */
 gulp.task('default', ['watch','serve']);
-gulp.task('transform', function(){
-  gulp.src(path.JS)
-    .pipe(plugins.react())
-    .pipe(gulp.dest(path.DEST_SRC));
-});
+
 gulp.task('copy', function(){
   gulp.src(path.HTML)
     .pipe(gulp.dest(path.DEST));
 });
 gulp.task('watch', function(){
-  gulp.watch(path.ALL, ['transform', 'copy']);
+  gulp.watch(path.HTML, ['copy']);
+
+  var watcher = watchify(browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+
+  return watcher.on('update', function(){
+    watcher.bundle()
+      .pipe(source(path.OUT))
+      .pipe(gulp.dest(path.DEST_SRC))
+      console.log('Updated');
+  })
+    .bundle()
+    .pipe(source(path.OUT))
+    .pipe(gulp.dest(path.DEST_SRC));
 });
 gulp.task('serve', function(){
   plugins.nodemon({
       script: 'index.js'
   });
 });
+
 /**
  * Production tasks
  */
 gulp.task('production', ['replaceHTML', 'build']);
+
 gulp.task('replaceHTML', function(){
   gulp.src(path.HTML)
     .pipe(plugins.htmlreplace({
